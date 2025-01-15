b8f618cd700bb738194279748ee6330a13215b09
Ope Johnson <66846985+Op3yem1@users.noreply.github.com>
2025-01-15 16:30:42 +0000

    Day 3, task 5 completed

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,9 @@
+#PAUSE 1 - What is 10 % 3?
+print(10 % 3)
+
+#PAUSE 2 - Check Odd or Even
+number = int(input("Enter a number to check "))
+if number % 2 == 0:
+    print(f"Number {number} is even")
+else:
+    print(f"Number {number} is odd")
