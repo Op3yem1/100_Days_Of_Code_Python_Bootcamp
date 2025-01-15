4e8f3894223556597a087adf01a5eb4b1c234104
Ope Johnson <66846985+Op3yem1@users.noreply.github.com>
2025-01-15 11:59:24 +0000

    Day 1 completed

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,2 +1,6 @@
-# Write your code below this line 👇
-print("Hello world!")
+print("Hello there! Welcome to the Band Generator\n")
+hometown = input("Where did you grow up? \n")
+pet = input("What was your childhood pet's name? \n")
+band_name = hometown + " " + pet
+
+print("Your band name would be: " + band_name)
