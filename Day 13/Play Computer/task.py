73aa29ebc2169ea2b38fffc408f11e18d435812e
Ope Johnson <66846985+Op3yem1@users.noreply.github.com>
2025-02-26 09:37:32 +0000

    Day 13 - Debugging Exercises completed

--- Day 13/Play Computer/task.py
+++ Day 13/Play Computer/task.py
@@ -1,6 +1,6 @@
 year = int(input("What's your year of birth?"))
 
-if year > 1980 and year < 1994:
+if year > 1980 and year <= 1994: #Pause 1: 1994 not accounted for in if statement. Change operator to <=
     print("You are a millennial.")
 elif year > 1994:
     print("You are a Gen Z.")
